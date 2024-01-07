dcf578d3c5907c309c77588c94b3eb5b110259f6
funMango <138420832+funMango@users.noreply.github.com>
2024-01-07 19:39:37 +0900

    Generate SwiftUI project

---
+++ Grocery-app/Grocery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_appApp.swift
+//  Grocery-app
+//
+//  Created by 이민호 on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Grocery-appUITests/Grocery_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Grocery_appUITestsLaunchTests.swift
+//  Grocery-appUITests
+//
+//  Created by 이민호 on 1/7/24.
+//
+
+import XCTest
+
+final class Grocery_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
